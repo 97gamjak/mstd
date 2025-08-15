e69ad6a6846fb1fd27ce00673ed1b873d93766f4
Jakob Gamper <97gamjak@gmail.com>
2025-08-15 08:39:56 +0200

    docs: add documentation to quantity class

--- include/mstd/units/quantity.hpp
+++ include/mstd/units/quantity.hpp
@@ -7,6 +7,17 @@
 namespace mstd
 {
 
+    /**
+     * @brief The quantity class
+     *
+     * @details This class represents a quantity with a value and a dimension
+     * associated with it. It is used to represent physical quantities such as
+     * length, mass, time, temperature, amount of substance, luminous intensity,
+     *
+     * @tparam T
+     * @tparam Dim
+     * @tparam Pack
+     */
     template <typename T, typename Dim, typename Pack>
     class Quantity
     {
@@ -42,6 +53,14 @@ namespace mstd
         T _si;
     };
 
+    /**
+     * @brief Constructs a Quantity from a value.
+     *
+     * @tparam T
+     * @tparam Dim
+     * @tparam Pack
+     * @param value The value to construct the Quantity from.
+     */
     template <typename T, typename Dim, typename Pack>
     constexpr Quantity<T, Dim, Pack>::Quantity(const T value)
         : _si(value * static_cast<T>(eff_scale::num) /
@@ -49,6 +68,15 @@ namespace mstd
     {
     }
 
+    /**
+     * @brief Constructs a Quantity from another Quantity.
+     *
+     * @tparam T
+     * @tparam Dim
+     * @tparam Pack
+     * @tparam OtherPack
+     * @param other The Quantity to construct the new Quantity from.
+     */
     template <typename T, typename Dim, typename Scale>
     template <typename OtherPack>
     constexpr Quantity<T, Dim, Scale>::Quantity(
@@ -58,6 +86,14 @@ namespace mstd
     {
     }
 
+    /**
+     * @brief Returns the value of the Quantity.
+     *
+     * @tparam T
+     * @tparam Dim
+     * @tparam Pack
+     * @return constexpr T
+     */
     template <typename T, typename Dim, typename Pack>
     constexpr T Quantity<T, Dim, Pack>::value() const
     {
@@ -65,6 +101,15 @@ namespace mstd
                static_cast<T>(eff_scale::num);
     }
 
+    /**
+     * @brief Adds two Quantities.
+     *
+     * @tparam T
+     * @tparam Dim
+     * @tparam Pack
+     * @param other The Quantity to add.
+     * @return constexpr Quantity
+     */
     template <typename T, typename Dim, typename Pack>
     template <typename OtherPack>
     constexpr Quantity<T, Dim, Pack> Quantity<T, Dim, Pack>::operator+(
@@ -74,6 +119,15 @@ namespace mstd
         return from_si(_si + other._si);
     }
 
+    /**
+     * @brief Subtracts two Quantities.
+     *
+     * @tparam T
+     * @tparam Dim
+     * @tparam Pack
+     * @param other The Quantity to subtract.
+     * @return constexpr Quantity
+     */
     template <typename T, typename Dim, typename Pack>
     template <typename OtherPack>
     constexpr Quantity<T, Dim, Pack> Quantity<T, Dim, Pack>::operator-(
@@ -83,6 +137,14 @@ namespace mstd
         return from_si(_si - other._si);
     }
 
+    /**
+     * @brief Constructs a Quantity from a n SI value.
+     *
+     * @tparam T
+     * @tparam Dim
+     * @tparam Pack
+     * @param value The value to construct the Quantity from.
+     */
     template <typename T, typename Dim, typename Pack>
     constexpr Quantity<T, Dim, Pack> from_si(const T si)
     {
@@ -91,6 +153,18 @@ namespace mstd
         return q;
     }
 
+    /**
+     * @brief friend operator* for Quantity
+     *
+     * @tparam T
+     * @tparam Dim1
+     * @tparam Pack1
+     * @tparam Dim2
+     * @tparam Pack2
+     * @param lhs
+     * @param rhs
+     * @return constexpr auto
+     */
     template <
         typename T,
         typename Dim1,
@@ -106,6 +180,18 @@ namespace mstd
         return Quantity<T, Dim1, Pack1>::from_si(lhs.si() * rhs.si());
     }
 
+    /**
+     * @brief friend operator/ for Quantity
+     *
+     * @tparam T
+     * @tparam Dim1
+     * @tparam Pack1
+     * @tparam Dim2
+     * @tparam Pack2
+     * @param lhs
+     * @param rhs
+     * @return constexpr auto
+     */
     template <
         typename T,
         typename Dim1,
