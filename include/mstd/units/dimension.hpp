cd3ce39dc4a13227874872d4e2d917cf517a53d8
Jakob Gamper <97gamjak@gmail.com>
2025-08-15 08:35:50 +0200

    docs: add documentation for dimension class

--- include/mstd/units/dimension.hpp
+++ include/mstd/units/dimension.hpp
@@ -7,31 +7,75 @@
 namespace mstd
 {
 
+    /**
+     * @brief the dimension class
+     *
+     * @details This class represents the physical dimensions of a quantity,
+     * including length, mass, time, temperature, amount of substance, luminous
+     * intensity, and electric current.
+     *
+     * @tparam L
+     * @tparam M
+     * @tparam T
+     * @tparam Th
+     * @tparam N
+     * @tparam I
+     * @tparam Cd
+     */
     template <int L, int M, int T, int Th, int N, int I, int Cd>
-    class dimension
+    struct dimension
     {
-       public:
         static constexpr std::size_t           rank = 7;
         static constexpr std::array<int, rank> exponents{L, M, T, Th, N, I, Cd};
     };
 
+    /**
+     * @brief implements dimension addition
+     *
+     * @tparam D1
+     * @tparam D2
+     * @tparam Is
+     * @return constexpr auto
+     */
     template <typename D1, typename D2, std::size_t... Is>
     constexpr auto dimension_add_impl(std::index_sequence<Is...>)
     {
         return dimension<D1::exponents[Is] + D2::exponents[Is]...>{};
     }
 
+    /**
+     * @brief implements dimension subtraction
+     *
+     * @tparam D1
+     * @tparam D2
+     * @tparam Is
+     * @return constexpr auto
+     */
     template <typename D1, typename D2, std::size_t... Is>
     constexpr auto dimension_sub_impl(std::index_sequence<Is...>)
     {
         return dimension<D1::exponents[Is] - D2::exponents[Is]...>{};
     }
 
+    /**
+     * @brief type alias for dimension addition
+     *
+     * @tparam D1
+     * @tparam D2
+     * @return constexpr auto
+     */
     template <typename D1, typename D2>
     using Dimension_add = decltype(dimension_add_impl<D1, D2>(
         std::make_index_sequence<D1::rank>{}
     ));
 
+    /**
+     * @brief type alias for dimension subtraction
+     *
+     * @tparam D1
+     * @tparam D2
+     * @return constexpr auto
+     */
     template <typename D1, typename D2>
     using Dimension_sub = decltype(dimension_sub_impl<D1, D2>(
         std::make_index_sequence<D1::rank>{}
