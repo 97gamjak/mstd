21f4782bf454dc9f3e3410a54eaa932f2c4ac283
Jakob Gamper <97gamjak@gmail.com>
2025-11-21 08:19:34 +0100

    feat: add GPL license headers, refactor C++ checks, and implement file type enumerations

--- scripts/mstd_cpp_checks/cpp_checks.py
+++ scripts/mstd_cpp_checks/cpp_checks.py
@@ -1,32 +1,58 @@
+/*****************************************************************************
+<GPL_HEADER>
+
+    mstd library
+    Copyright (C) 2025-now  Jakob Gamper
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+<GPL_HEADER>
+******************************************************************************/
+
 """Module defining C++ check rules."""
 
+import logging
+import sys
+
+from cpp_rules import cpp_rules
 from enums import FileType
+from files import __BASE_DIR__, get_files_in_dirs, get_staged_files
+from rules import check_file
 
-from cpp_checks import ResultType, check_key_sequence_ordered
+__CPP_DIRS__ = ["include", "test"]
+__OTHER_DIRS__ = ["scripts"]
+__EXCLUDE_DIRS__ = ["__pycache__", ".ruff_cache"]
+__EXCLUDE_FILES__ = [".gitignore"]
 
+__DIRS__ = __CPP_DIRS__ + __OTHER_DIRS__
 
-def static_constexpr_inline_rule(line: str) -> ResultType:
-    """Check for usage of static, constexpr, and inline in variable declarations.
-
-    Parameters
-    ----------
-    line: str
-        The line of code to check.
-
-    Returns
-    -------
-    cpp_types.ResultType:
-        Result of the check, Warning if all keywords are not present, Ok otherwise
-
-    """
-    key_sequence = ["static", "inline", "constexpr"]
-    return check_key_sequence_ordered(key_sequence, line)
+cpp_check_logger = logging.getLogger("mstd_cpp_checks")
 
 
-rules = []
-rules.append(
-    [
-        {FileType.CPPHeader, FileType.CPPSource},
-        static_constexpr_inline_rule
-    ]
-)
+def run_checks(rules: list[set[FileType], callable]) -> None:
+
+    if "full" in sys.argv:
+        cpp_check_logger.info("Running full checks...")
+        dirs = [__BASE_DIR__ / dir_name for dir_name in __DIRS__]
+        files = get_files_in_dirs(dirs, __EXCLUDE_DIRS__, __EXCLUDE_FILES__)
+    else:
+        cpp_check_logger.info("Running checks on staged files...")
+        files = get_staged_files()
+
+    for filename in files:
+        check_file(filename, rules)
+
+
+if __name__ == "__main__":
+    run_checks(cpp_rules)
--- scripts/mstd_cpp_checks/files.py
+++ scripts/mstd_cpp_checks/files.py
@@ -1,16 +1,41 @@
+/*****************************************************************************
+<GPL_HEADER>
+
+    mstd library
+    Copyright (C) 2025-now  Jakob Gamper
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+<GPL_HEADER>
+******************************************************************************/
+
 """Module for file operations in C++ checks."""
 
+import subprocess
 from pathlib import Path
 
 from enums import FileType
 
+__BASE_DIR__ = Path(__file__).parent.parent.parent.resolve()
 
-def determine_file_type(filename: str) -> FileType:
+
+def determine_file_type(filename: str | Path) -> FileType:
     """Determine the file type based on the filename extension.
 
     Parameters
     ----------
-    filename: str
+    filename: str | Path
         The name of the file to check.
 
     Returns
@@ -19,6 +44,8 @@ def determine_file_type(filename: str) -> FileType:
         The determined file type.
 
     """
+    filename = str(filename)
+
     if filename.endswith((".h", ".hpp")):
         return FileType.CPPHeader
     if filename.endswith((".cpp", ".cxx", ".cc", ".c")):
@@ -26,34 +53,68 @@ def determine_file_type(filename: str) -> FileType:
     if filename == "CMakeLists.txt":
         return FileType.CMakeLists
 
-    msg = f"Unknown file type for filename: {filename}"
-    raise ValueError(msg)
+    return FileType.UNKNOWN
 
 
-def get_files_in_dirs(dirs: list[str]) -> list[str]:
+def get_files_in_dirs(
+    paths: list[Path],
+    exclude_dirs: list[str] | None = None,
+    exclude_files: list[str] | None = None
+) -> list[Path]:
     """Get all files in the specified directories.
 
     Parameters
     ----------
-    dirs: list[str]
+    paths: list[Path]
         List of directory paths to search for files.
+    exclude_dirs: list[str] | None
+        List of directory names to exclude from the search. Defaults to None.
+    exclude_files: list[str] | None
+        List of file names to exclude from the search. Defaults to None.
+
+    Returns
+    -------
+    list[Path]:
+        List of file paths found in the specified directories.
+
+    """
+    if exclude_dirs is None:
+        exclude_dirs = []
+
+    if exclude_files is None:
+        exclude_files = []
+
+    all_files = []
+    for path in paths:
+        if path.is_dir() and path.name not in exclude_dirs:
+            all_files.extend(
+                get_files_in_dirs(path.iterdir(), exclude_dirs, exclude_files)
+            )
+        elif path.is_file() and path.name not in exclude_files:
+            all_files.append(path)
+
+    return all_files
+
+
+def get_staged_files() -> list[str]:
+    """Get the list of staged files in the git repository.
 
     Returns
     -------
     list[str]:
-        List of file paths found in the specified directories.
+        List of staged file paths.
 
     """
-    all_files = []
+    result = subprocess.run(
+        ["git", "diff", "--name-only", "--cached"],
+        capture_output=True,
+        text=True,
+        check=True
+    )
 
-    for dir_path in dirs:
-        path_obj = Path(dir_path)
-        if not path_obj.is_dir():
-            msg = f"Provided path {dir_path} is not a directory."
-            raise NotADirectoryError(msg)
+    if result.returncode != 0:
+        msg = "Failed to get staged files from git."
+        raise RuntimeError(msg)
 
-        for file_path in path_obj.rglob("*"):
-            if file_path.is_file():
-                all_files.extend([str(file_path)])
-
-    return all_files
+    files = result.stdout.strip().split("\n")
+    return [file for file in files if file]
